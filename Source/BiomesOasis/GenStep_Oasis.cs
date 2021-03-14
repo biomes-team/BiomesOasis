63a034362150120481ed18e205a6c56157edf8c3
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-03-14 12:12:33 -0600

    Pushed hills out of the water on oasis maps. Misc minor cleanup

--- Source/BiomesOasis/GenStep_Oasis.cs
+++ Source/BiomesOasis/GenStep_Oasis.cs
@@ -12,7 +12,7 @@ using BiomesCore.DefModExtensions;
 
 namespace BiomesOasis.GenSteps
 {
-    public class GenStep_Oasis : GenStep
+    public class GenStep_OasisB : GenStep
     {
         public override int SeedPart
         {
@@ -22,13 +22,13 @@ namespace BiomesOasis.GenSteps
             }
         }
 
-        float oasisSize = Rand.Range(30f, 70f);
+        float oasisBaseSize = Rand.Range(30f, 30f);
         float beachSize = Rand.Range(30f, 50f);
         float distanceVariance = Rand.Range(1.0f, 1.5f);
         float perlinVariance = 5f;
         bool isIsland;
         List<int> tmpNeighbors = new List<int>();
-        int i = 0;
+
         public override void Generate(Map map, GenStepParams parms)
         {
             if (!map.Biome.HasModExtension<BiomesMap>())
@@ -42,94 +42,71 @@ namespace BiomesOasis.GenSteps
             MapGenFloatGrid oasisGrid = MapGenerator.FloatGridNamed("OasisGrid");
             IntVec3 oasisCenter = map.Center;
             ModuleBase moduleBase = new Perlin(Rand.Range(0.015f, 0.028f), 2.0, 0.5, 6, Rand.Range(0, 2147483647), QualityMode.Medium);
-            if (oasisSize >= 50f)
+            if (oasisBaseSize >= 50f)
             {
                 perlinVariance = 6f;
             }
             WorldGrid grid = Find.World.grid;
             int tileID = map.Tile;
             grid.GetTileNeighbors(tileID, tmpNeighbors);
-            for (int count = tmpNeighbors.Count; i < count; i++)
+            for (int i = 0; i < tmpNeighbors.Count; i++)
             {
                 if (grid[tmpNeighbors[i]].biome != BiomeDefOf.Ocean)
                 {
                     isIsland = false;
                 }
             }
-            Rot4 rot4 = Find.World.CoastDirectionAt(map.Tile);
-            if (isIsland != false)
-            {
-                oasisSize = Rand.Range(20f, 30f);
-                perlinVariance = 4f;
-            }
-            else if (rot4 == Rot4.North)
-            {
-                oasisCenter.z -= 10;
-                oasisSize = Rand.Range(20f, 40f);
-                perlinVariance = 4f;
-            }
-            else if (rot4 == Rot4.South)
-            {
-                oasisCenter.z += 10;
-                oasisSize = Rand.Range(20f, 40f);
-                perlinVariance = 4f;
-            }
-            else if (rot4 == Rot4.East)
-            {
-                oasisCenter.x -= 10;
-                oasisSize = Rand.Range(20f, 40f);
-                perlinVariance = 4f;
-            }
-            else if (rot4 == Rot4.West)
-            {
-                oasisCenter.x += 10;
-                oasisSize = Rand.Range(20f, 40f);
-                perlinVariance = 4f;
-            }
+            Rot4 beachDirection = Find.World.CoastDirectionAt(map.Tile);
             if (isIsland == true)
             {
-                oasisSize = Rand.Range(20f, 30f);
+                //oasisBaseSize = Rand.Range(20f, 30f);
+                perlinVariance = 4f;
             }
-            MapGenFloatGrid elevation = MapGenerator.Elevation;
-            foreach (IntVec3 current in map.AllCells)
+
+            else
             {
-                elevation[current] = elevation[current] * 0.75f;
-                float distance = DistanceBetweenPoints(current, oasisCenter) / 1.5f;
-                oasisGrid[current] = ((moduleBase.GetValue(current) * perlinVariance ) + 0.1f * ((oasisSize) - (distance * distanceVariance))) - (elevation[current] * 11);
-            }
-            TerrainGrid terrainGrid = map.terrainGrid;
-            TerrainDef richSoil = TerrainDef.Named("SoilRich");
-            TerrainDef soil = TerrainDef.Named("Soil");
-            MapGenFloatGrid fertility = MapGenerator.Fertility;
-            foreach (IntVec3 current in map.AllCells)
-            {
-                // leaves mountains & most surrounding gravel untouched, if there is any mountain in the oasis area
-                // otherwise, mountains get water placed under them
-                if (elevation[current] < 0.65f)                         
+                if(beachDirection != null)
                 {
-                    if (!terrainGrid.TerrainAt(current).IsRiver)
+                    // If it has a beach, the oasis is smaller with reduced variance
+                    // Move the center of the oasis away from the beach
+                    //oasisBaseSize = Rand.Range(20f, 30f);
+                    perlinVariance = 4f;
+                    if (beachDirection == Rot4.North)
                     {
-                        if (oasisGrid[current] > 0f)
-                        {
-                            terrainGrid.SetTerrain(current, TerrainDefOf.WaterShallow);
-                            if (oasisGrid[current] > 2)
-                            {
-                                terrainGrid.SetTerrain(current, TerrainDefOf.WaterDeep);
-                            }
-                        }
-                        else if (oasisGrid[current] > 0f - 0.1f * beachSize)
-                        {
-                            if(oasisGrid[current] + 0.1 * fertility[current] > 1f - 0.1f * (beachSize * 0.8))
-                            {
-                                terrainGrid.SetTerrain(current, richSoil);
-                            }
-                            else
-                            {
-                                terrainGrid.SetTerrain(current, soil);
-                            }
-                        }
+                        oasisCenter.z -= 10;
+                    }
+                    else if (beachDirection == Rot4.South)
+                    {
+                        oasisCenter.z += 10;
+                    }
+                    else if (beachDirection == Rot4.East)
+                    {
+                        oasisCenter.x -= 10;
+                    }
+                    else if (beachDirection == Rot4.West)
+                    {
+                        oasisCenter.x += 10;
                     }
                 }
+            }
+            
+            MapGenFloatGrid elevation = MapGenerator.Elevation;
+            MapGenFloatGrid fertility = MapGenerator.Fertility;
+            Log.Message("Map size:" + map.Size.x);
+            float oasisSize = (oasisBaseSize / 10) * (map.Size.x / 10);
+            float perlin;
+            Log.Message("oasisSize:  " + oasisSize);
+
+            foreach (IntVec3 current in map.AllCells)
+            {
+                float distance = (float)Math.Pow((DistanceBetweenPoints(oasisCenter, current) / 6), 2) + 10;
+                perlin = ((moduleBase.GetValue(current) + 1) * 7);
+                float elevationWeight = 20 * elevation[current];
+                float elevationPerlin = (((moduleBase.GetValue(current) + 1) * 10) * elevation[current]);
+                fertility[current] = 1 + ((oasisSize) * elevationPerlin) / distance;
+
+                // this pushes the hills out of the lake
+                elevation[current] += 0.3f - fertility[current] * 0.1f;
             }
         }
 
