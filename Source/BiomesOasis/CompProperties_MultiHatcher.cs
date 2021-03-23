d146821fc5eb0ae449b901bd6f564ea17d5407fb
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-03-22 19:11:48 -0600

    Tarimtula implemented

---
+++ Source/BiomesOasis/CompProperties_MultiHatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace BiomesOasis
+{
+    public class CompProperties_MultiHatcher : CompProperties_Hatcher
+    {
+        public IntRange numToHatch = new IntRange(3, 5);
+        public CompProperties_MultiHatcher()
+        {
+            compClass = typeof(CompMultiHatcher);
+        }
+    }
+}
